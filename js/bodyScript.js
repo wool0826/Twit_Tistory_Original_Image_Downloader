d2ad8a938a80a4a906fc8dc395befc8834887e3d
wool0826 <chanwool94@gmail.com>
2021-05-15 20:44:50 +0900

    [#3] Refactoring code

--- js/bodyScript.js
+++ js/bodyScript.js
@@ -1,16 +1,16 @@
 function save_options(){
-    const selection = document.getElementById("hotkeys");
-    const cHotkeyOption = selection[selection.selectedIndex].value;
+    const singleHotkey = document.getElementById("hotkeys");
+    const currentSingleHotkey = singleHotkey[singleHotkey.selectedIndex].value;
 
     const sort = document.getElementById("sort_method");
     const sortOption = sort[sort.selectedIndex].value;
 
     chrome.storage.local.set({
-        hotkeyOption: cHotkeyOption,
+        hotkeyOption: currentSingleHotkey,
         sortOption: sortOption
     }, function(){
         var status = document.getElementById("status");
-        status.textContent = "저장되었습니다.(saved)";
+        status.textContent = "Saved";
         setTimeout(function(){
             status.textContent = "";
         }, 750);
@@ -22,17 +22,17 @@ function restore_options(){
         hotkeyOption: "None",
         sortOption: "None"
     }, function(items){
-        var selection = document.getElementById("hotkeys");
-        for(var i=0; i<selection.length; i++){
-            if(selection[i].value==items.hotkeyOption){
-                selection.selectedIndex = i;
+        var singleHotkey = document.getElementById("hotkeys");
+        for(var i=0; i<singleHotkey.length; i++){
+            if(singleHotkey[i].value == items.hotkeyOption){
+                singleHotkey.selectedIndex = i;
                 break;
             }
         }
-
+        
         var sort = document.getElementById("sort_method");
         for(var i=0; i<sort.length; i++){
-            if(sort[i].value==items.sortOption){
+            if(sort[i].value == items.sortOption){
                 sort.selectedIndex = i;
                 break;
             }
